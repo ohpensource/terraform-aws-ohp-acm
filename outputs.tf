a4e19a4981deb4a5770a76402354ade22f707a50
Rupert <rbroad@ohpen.com>
2021-04-29 14:10:01 +0200

    lanz-593 tfm fmt

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "The ARN of the certificate"
-  value = aws_acm_certificate.main.id
+  value       = aws_acm_certificate.main.id
 }
 
 output "arn" {
   description = "The ARN of the certificate"
-  value = aws_acm_certificate.main.arn
+  value       = aws_acm_certificate.main.arn
 }
 
 output "status" {
   description = "Status of the certificate"
-  value = aws_acm_certificate.main.status
+  value       = aws_acm_certificate.main.status
 }
